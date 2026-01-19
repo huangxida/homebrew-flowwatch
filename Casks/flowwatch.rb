d1f4c15caa490de965dced5ecf1e31e8688e5379
huangxida <a544358575@gmail.com>
2026-01-19 15:12:48 +0800

    Initial commit

---
+++ Casks/flowwatch.rb
@@ -0,0 +1,19 @@
+cask "flowwatch" do
+  version "0.0.3"
+  sha256 "c8a645c8bfdb9809b3b6ec1a5f45dff74d94dc073005dd23bc652fa800697b6b"
+
+  url "https://github.com/huangxida/FlowWatch/releases/download/v#{version}/FlowWatch.dmg"
+  name "FlowWatch"
+  desc "Menu bar network speed monitor and traffic stats"
+  homepage "https://github.com/huangxida/FlowWatch"
+
+  depends_on macos: ">= :ventura"
+
+  app "FlowWatch.app"
+
+  zap trash: [
+    "~/Library/Application Support/FlowWatch",
+    "~/Library/Containers/com.hxd.FlowWatch",
+    "~/Library/Preferences/com.hxd.FlowWatch.plist",
+  ]
+end
